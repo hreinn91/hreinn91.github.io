b422b591532b10259126ec48c794c237f8189d95
hreinn91 <hreinnjuliusson@gmail.com>
2023-03-12 21:47:23 +0100

    Add react flow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactFlow, { Controls, Background } from 'reactflow';
+import 'reactflow/dist/style.css';
 
-function App() {
+function Flow() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100%' }}>
+      <ReactFlow>
+        <Background />
+        <Controls />
+      </ReactFlow>
     </div>
   );
 }
 
-export default App;
+export default Flow;
