0ea8f505f8632aa5a193223488e29fc560fcc4f6
hreinn91 <hreinnjuliusson@gmail.com>
2024-02-23 22:23:47 +0100

    Internets backwater

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Welcome to the back water of the internet</p>
+        <a 
+        className='App-link'
+        href='https://google.com'
+        >Go back to google</a>
+        </header>
     </div>
   );
 }
