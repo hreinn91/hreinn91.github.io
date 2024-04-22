80e8a4567d55a4d7cc92d100774a919d29b349ad
hreinn91 <hreinnjuliusson@gmail.com>
2024-04-22 23:30:14 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ const router = createHashRouter([
     ]
   },
   {
+    path: "/rultenpastinsen",
+    element: <BeerSpace />
+  },
+  {
     path: "/beerspace",
     element: <BeerSpace />
   },
